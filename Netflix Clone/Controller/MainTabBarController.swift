17e5ceedfce7309df2757f662b164b3e5286c2ac
Ahmed Yamany <ahmed.mohammed.yamany@gmail.com>
2022-08-31 16:48:05 +0200

    initial commit & change the scene rootViewController

---
+++ Netflix Clone/Controller/MainTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Ahmed Yamany on 31/08/2022.
+//
+
+import UIKit
+
+
+
+class MainTabBarController: UITabBarController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+
+
+}
+
